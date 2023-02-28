1282cf1ec685be00922ae98ee4986126ce6f25ee
Ruslan Pliev <ruslan-pliev@mail.ru>
2023-02-28 18:49:52 +0300

    Second commit

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,15 @@
 package koschei.models;
 
-public class Needle7 {
+import org.springframework.stereotype.Component;
 
+@Component
+public class Needle7 {
+private Deth8 pepsi;
+public Needle7(Deth8 pepsi){
+    this.pepsi=pepsi;
+}
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле :( " + pepsi.toString();
     }
 }
